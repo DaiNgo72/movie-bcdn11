182f9d5a5ce3d882d6caf27fb2d203e9f5269935
DaiNgo <daingo7211@gmail.com>
2024-07-25 21:51:36 +0700

    Start - Lazy

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,4 +1,12 @@
-import { createBrowserRouter, Link } from "react-router-dom";
+import { createBrowserRouter, Link, Navigate } from "react-router-dom";
+import { lazy, Suspense } from "react";
+
+// import Movie from "../atomic/pages/movie";
+// import Anime from "../atomic/pages/anime";
+
+// Lazy load
+const Movie = lazy(() => import("../atomic/pages/movie"));
+const Anime = lazy(() => import("../atomic/pages/anime"));
 
 export const router = createBrowserRouter([
   {
@@ -6,12 +14,43 @@ export const router = createBrowserRouter([
     element: (
       <div>
         <h1>Hello Cyber</h1>
-        <Link to="about">About Us</Link>
+        <Link to="movie">Movie</Link>
+        <Link to="anime">Anime</Link>
       </div>
     ),
   },
+
   {
-    path: "about",
-    element: <div>About</div>,
+    path: "movie",
+    element: (
+      // Đợi page của chúng ta tải xong rồi mới sử dụng để render.
+      // fallback: render tạm thời khi page chưa tải xong
+      <Suspense fallback={<>Loading....</>}>
+        <Movie />
+      </Suspense>
+    ),
+  },
+
+  {
+    path: "tv-series",
+    element: <>tv-series</>,
+  },
+
+  {
+    path: "anime",
+    element: (
+      <Suspense fallback={<>Loading....</>}>
+        <Anime />
+      </Suspense>
+    ),
+  },
+
+  {
+    path: "*",
+    // C1: Custom Page Not Found
+    // element: <>Not Found</>,
+
+    // C2: Mong muốn chuyển về trang home
+    element: <Navigate to={"/"} replace />,
   },
 ]);
